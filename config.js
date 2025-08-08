a0fdc3e5d7db3ab00cc0c06cc9d3cb2f17b2a109
nasirjan244888-lab <nasirjan244888@gmail.com>
2025-08-08 14:15:40 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
+  SESSION_ID: "drakonis~Ijdj3TpR#RF-sTJFbyOAkJ4cbChoR89kJrjEJ4GYTHGs4CTZLhRg",  // add your Session Id make sure it starts with 
   OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
